ab64b585f3bf47b818dccb374ee7cde18386f7d5
franciscofl12 <xiskoyruben@gmail.com>
2020-10-29 09:53:12 +0100

    Primeros pasos del juego

--- src/me/juegotrivia/Main.java
+++ src/me/juegotrivia/Main.java
@@ -23,13 +23,39 @@ public class Main {
 	}
 	
 	public static void menu() {
-		System.out.println("Menu de seleccion\n"
-				+ "1. Jugar\n"
-				+ "2. Contador de puntos\n"
-				+ "0. Salir del juego");
+		boolean menu = false ;
+		
+		while (menu==false) {
+			menu = false;
+			System.out.println("Menu de seleccion\n"
+					+ "1. Jugar\n"
+					+ "2. Contador de puntos\n"
+					+ "0. Salir del juego");
+			
+			String seleccion = JOptionPane.showInputDialog("Selecciona accion.");
+			int seleccionmenu = Integer.parseInt(seleccion);
+			
+			if (seleccionmenu == 1) {
+				menu = true;
+				juego();
+			}
+			else {
+				if (seleccionmenu == 2) {
+					menu = true;
+				
+				}
+				else {
+					if (seleccionmenu == 3) {
+						menu = true;
+						
+					}
+				}
+			}
+		}
 	}
 	
 	public static void juego() {
+		
 		System.out.println("El juego va a comenzar.\n\n"
 				+ "1. Hardware PC\n"
 				+ "2. Software PC\n"
@@ -39,6 +65,7 @@ public class Main {
 		int seleccionum = Integer.parseInt(seleccion);
 		
 		if (seleccionum == 1) {
+			hardware();
 		}
 		else {
 			if (seleccionum == 2) {
@@ -50,11 +77,35 @@ public class Main {
 					juego();
 				}
 			}
-					
+		}	
+	}
+	
+	public static void hardware() {
+		int hardware = 0;
+		boolean confirmacion = false;
+		
+		// Primera pregunta de Hardware
+		while (confirmacion==false) {
+			confirmacion=false;
+			System.out.println("Primera pregunta :\n"
+				+ "¿Es verdad que los RGB te dan mas FPS?\n"
+				+ "(A) : Verdadero\n"
+				+ "(B) : Falso");
+		
+			String respuesta = JOptionPane.showInputDialog("Introduzca su respuesta: ");
+			respuesta = respuesta.toUpperCase();
 			
-			
-				
+			if (respuesta=="A") {
+				confirmacion=true;
+				System.out.println("Ha seleccionado la respuesta A.");
+				hardware = hardware + 1;
+			}
+			else {
+				if (respuesta=="B") {
+					confirmacion=true;
+					System.out.println("Ha seleccionado la respuesta B");
+				}
+			}
 		}
-			
 	}
 }
